60bb43408a02176e55966b9d2a812d71afad8f89
Sven Wagner <svewag@gmail.com>
2011-07-07 00:28:44 +0200

    look & feel, history

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var http = require("http");
 var URL = require("url");
 var exec = require('child_process').exec;
 
+var user = "unknown";
+var hostname = "unknown";
+
+exec("whoami", function(error, stdout, stderr){
+	user = stdout.replace(/\n/, "");
+});
+
+exec("hostname", function(error, stdout, stderr){
+	hostname = stdout.replace(/\n/, "");
+});
+
 http.createServer(function(request, response){
 	var query = URL.parse(request.url, true).query;
 	if(!query){ response.end(); return; }
@@ -12,15 +23,22 @@ http.createServer(function(request, response){
 	if(cmd){
 		sys.puts("exec " + cmd);
 		exec(cmd, function(error, stdout, stderr){
-			response.writeHead(200, {"Content-Type": "application/json"});
-			sys.puts("result: "+ stdout);
-			var hash = {
-				"output": stdout
-			};
-			var data = callback + "([" + JSON.stringify(hash) + "])";
-			sys.puts(data);
-			response.write(data);
-			response.end();
+			exec("pwd", function(err, path){
+				path = path.replace(/\n/, "");
+				response.writeHead(200, {"Content-Type": "application/javascript"});
+				var result = stdout || stderr;
+				sys.puts("result: "+ result);
+				var hash = {
+					"hostname": hostname,
+					"user": user,
+					"path": path,
+					"output": result,
+				};
+				var data = callback + "([" + JSON.stringify(hash) + "])";
+				sys.puts(data);
+				response.write(data);
+				response.end();
+			});
 		});
 	}
 }).listen(8000);
